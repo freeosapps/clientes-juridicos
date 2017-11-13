3477056ac0c13e4c9cdab612bc6f5143bc29ece4
brunomrsn <brunomrsn@gmail.com>
2017-11-13 11:24:00 -0200

    Ampliação e redução de altura e largura

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 class Tabela {
   constructor() {
+    this.largura = 1;
     this.celulas = [[new Celula()]];
+  }
+  ampliarLargura() {
+    for (let i = 0; i < this.celulas.length; i++) {
+      this.celulas[i].push(new Celula());
+    }
+    this.largura++;
+  }
+  reduzirLargura() {
+    if (this.largura > 1) {
+      for (let i = 0; i < this.celulas.length; i++) {
+        this.celulas[i].splice(this.celulas[i].length - 1, 1);
+      }
+      this.largura--;
+    }
+  }
+  percorrerLargura(indice, callback) {
+    for (let i; i < this.largura; i++) {
+      callback(this.celulas[indice][i]);
+    }
+  }
+  percorrerAltura(indice, callback) {
+    for (let i; i < this.celulas.length; i++) {
+      callback(this.celulas[i][indice]);
+    }
   }
   ampliarAltura() {
     let altura = this.celulas.length;
     for (let i = 0; i <= altura; i++) {
       if (this.celulas[i] === undefined) {
-        this.celulas[i] = [new Celula()];
-      }
-    }
-  }
-  ampliarLargura() {
-    let altura = this.celulas.length;
-    for (let i = 0; i < altura; i++) {
-      let largura = this.celulas[i].length;
-      for (let j = 0; j <= largura; j++) {
-        if (this.celulas[i][j] === undefined) {
-          this.celulas[i][j] = new Celula();
+        this.celulas[i] = [];
+        for (let j = 0; j < this.largura; j++) {
+          this.celulas[i].push(new Celula());
         }
       }
     }
   }
-
+  reduzirAltura() {
+    let altura = this.celulas.length;
+    if (altura > 1) {
+      this.celulas.splice(altura - 1, 1);
+    }
+  }
   desenhar() {
     let tabela = $('<div>');
     for (let i = 0; i < this.celulas.length; i++) {
@@ -41,17 +63,17 @@ class Tabela {
 
 class Celula {
   contem(conteudo) {
-
   }
   desenhar() {
     let span = $('<span>');
     span.css({
-      borderStyle: 'solid',
-      borderWidth: 1,
       flex: 1,
       textAlign: 'center'
     });
     let input = $('<input>');
+    input.css({
+      width: '100%'
+    });
     span.append(input);
     return span;
   }
@@ -62,20 +84,42 @@ $(() => {
   let containerTabela = $('<div>');
   containerTabela.append(tabela.desenhar());
   $(document.body).append(containerTabela);
+
   let ampliarLargura = $('<button>');
   ampliarLargura.on('click', () => {
     tabela.ampliarLargura();
     containerTabela.empty();
     containerTabela.append(tabela.desenhar());
   });
-  ampliarLargura.text('Ampliar largura');
+  ampliarLargura.text('+ largura');
   $(document.body).append(ampliarLargura);
+  
+  let reduzirLargura = $('<button>');
+  reduzirLargura.on('click', () => {
+    tabela.reduzirLargura();
+    containerTabela.empty();
+    containerTabela.append(tabela.desenhar());
+  });
+  reduzirLargura.text('- largura');
+  $(document.body).append(reduzirLargura);
+
   let ampliarAltura = $('<button>');
   ampliarAltura.on('click', () => {
     tabela.ampliarAltura();
     containerTabela.empty();
     containerTabela.append(tabela.desenhar());
   });
-  ampliarAltura.text('Ampliar altura');
+  ampliarAltura.text('+ altura');
   $(document.body).append(ampliarAltura);
+
+  let reduzirAltura = $('<button>');
+  reduzirAltura.on('click', () => {
+    tabela.reduzirAltura();
+    containerTabela.empty();
+    containerTabela.append(tabela.desenhar());
+  });
+  reduzirAltura.text('- altura');
+  $(document.body).append(reduzirAltura);
+
+
 });
